dddadd9d6b9801c1f4d46414e8660c4a51718f7b
Noceo200 <onoel2050@gmail.com>
2023-12-07 00:23:17 +0900

    Update

---
+++ robot_ws_ros2/src/icps_bot/src/command_master.cpp
@@ -0,0 +1,122 @@
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
+bool is_command(geometry_msgs::msg::Twist msg){
+    bool result = msg.linear.x != 0 || msg.linear.y != 0 || msg.angular.z != 0;
+    return result;
+}
+
+bool is_same(geometry_msgs::msg::Twist prev_msg,geometry_msgs::msg::Twist new_msg){
+    bool result = prev_msg.linear.x == new_msg.linear.x && prev_msg.linear.y == new_msg.linear.y && prev_msg.angular.z == new_msg.angular.z;
+    return result;
+}
+
+void init_twist(geometry_msgs::msg::Twist &msg){
+    msg.linear.x = 0.0;
+    msg.linear.y = 0.0;
+    msg.linear.z = 0.0;
+    msg.angular.x = 0.0;
+    msg.angular.y = 0.0;
+    msg.angular.z = 0.0;
+}
+
+class CommandMasterNode : public rclcpp::Node
+{
+public:
+  CommandMasterNode() : Node("command_master_node")
+  {
+    // Initialize subscribers
+    sub_teleop_key = create_subscription<geometry_msgs::msg::Twist>(
+      "cmd_vel_teleop_key", 10, std::bind(&CommandMasterNode::twistCallback_key, this, std::placeholders::_1));
+
+    sub_teleop_joy = create_subscription<geometry_msgs::msg::Twist>(
+      "cmd_vel_teleop_joy", 10, std::bind(&CommandMasterNode::twistCallback_joy, this, std::placeholders::_1));
+
+    sub_nav = create_subscription<geometry_msgs::msg::Twist>(
+      "cmd_vel_nav", 10, std::bind(&CommandMasterNode::twistCallback_nav, this, std::placeholders::_1));
+
+    // Initialize publisher
+    pub_command = create_publisher<geometry_msgs::msg::Twist>("cmd_vel_apply", 10);
+
+    // Initialize variables to store received twist messages
+    init_twist(twist_teleop_key);
+    init_twist(twist_teleop_joy);
+    init_twist(twist_nav);
+
+    // Set a timer to publish the selected twist message periodically
+    timer_ = create_wall_timer(std::chrono::milliseconds(10), std::bind(&CommandMasterNode::publishTwist, this));
+  }
+
+private:
+  void twistCallback_key(const geometry_msgs::msg::Twist::SharedPtr msg)
+  {
+    //MAIN CODE to get commands from keyboard
+    if(!is_same(twist_teleop_key,*msg)){twist_teleop_key = *msg;}
+    teleop_key_active=0;
+  }
+
+  void twistCallback_joy(const geometry_msgs::msg::Twist::SharedPtr msg)
+  {
+    //MAIN CODE to get commands from joystick
+    if(!is_same(twist_teleop_joy,*msg)){twist_teleop_joy= *msg;}
+    teleop_joy_active=0;
+  }
+  
+  void twistCallback_nav(const geometry_msgs::msg::Twist::SharedPtr msg)
+  {
+    //MAIN CODE to get commands from nav2 plugin
+    if(!is_same(twist_nav,*msg)){twist_nav = *msg;} 
+    nav_active =0;  
+  }
+
+  void publishTwist()
+  {
+    geometry_msgs::msg::Twist selected_twist;
+
+    //check if a subscriber is active
+    if(teleop_key_active >= iteration_to_reset){init_twist(twist_teleop_key);}
+    if(teleop_joy_active >= iteration_to_reset){init_twist(twist_teleop_joy);}
+    if(nav_active >= iteration_to_reset){init_twist(twist_nav);}
+
+    teleop_key_active ++;
+    teleop_joy_active ++;
+    nav_active ++;
+
+    //choice of linear commands to apply
+    if(is_command(twist_teleop_key)){selected_twist=twist_teleop_key;}
+    else if(is_command(twist_teleop_joy)){selected_twist=twist_teleop_joy;}
+    else if(is_command(twist_nav)){selected_twist=twist_nav;}
+    else{
+        init_twist(selected_twist);
+    }
+
+    // Publish the selected twist message
+    pub_command->publish(selected_twist);
+  }
+
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_teleop_key; 
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_teleop_joy;
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_nav;
+
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_command;
+
+  geometry_msgs::msg::Twist twist_teleop_key;
+  geometry_msgs::msg::Twist twist_teleop_joy;
+  geometry_msgs::msg::Twist twist_nav;
+
+// Flags to indicate if new messages have been received on each topic, if no message have been received for an amount of time, we reset the command.
+  int teleop_key_active = 0;
+  int teleop_joy_active = 0;
+  int nav_active = 0;
+  int iteration_to_reset = 100; //1s if publisher frequence is 10ms
+
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char *argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<CommandMasterNode>());
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ robot_ws_ros2/src/icps_bot/src/simu_bot_driver.cpp
@@ -0,0 +1,55 @@
+#include "rclcpp/rclcpp.hpp"
+#include "trajectory_msgs/msg/joint_trajectory.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
+bool is_same(geometry_msgs::msg::Twist prev_msg,geometry_msgs::msg::Twist new_msg){
+    bool result = prev_msg.linear.x == new_msg.linear.x && prev_msg.linear.y == new_msg.linear.y && prev_msg.angular.z == new_msg.angular.z;
+    return result;
+}
+
+class SimuBotDriver : public rclcpp::Node {
+public:
+  SimuBotDriver() : Node("simu_bot_driver_node") {
+    publisher_cmd = create_publisher<trajectory_msgs::msg::JointTrajectory>("/set_joint_trajectory", 10);
+    subscriber_cmd = create_subscription<geometry_msgs::msg::Twist>("/cmd_vel_apply", 10, std::bind(&SimuBotDriver::twistCallback, this, std::placeholders::_1));
+
+
+    timer_ = create_wall_timer(std::chrono::milliseconds(100), std::bind(&SimuBotDriver::publishJointTrajectory, this));
+
+  }
+
+private:
+  void twistCallback(const geometry_msgs::msg::Twist::SharedPtr msg)
+  {
+    if(!is_same(cmd_vel,*msg)){cmd_vel = *msg;}
+  }
+
+  void publishJointTrajectory() {
+    auto message = std::make_unique<trajectory_msgs::msg::JointTrajectory>();
+    message->header.frame_id = "base_link";
+    message->joint_names = {"wheel_f_sup_joint", "wheel_bl_sup_joint", "wheel_br_sup_joint", "wheel_f_joint", "wheel_bl_joint", "wheel_br_joint"};
+    trajectory_msgs::msg::JointTrajectoryPoint point;
+    //convert linear and angular command into ones adapted to robot
+    point.positions = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    cmd_vel.linear.x
+    cmd_vel.linear.y
+    cmd_vel.angular.z
+
+    message->points.push_back(point);
+
+    publisher_cmd->publish(std::move(message));
+  }
+
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscriber_cmd; 
+  rclcpp::Publisher<trajectory_msgs::msg::JointTrajectory>::SharedPtr publisher_cmd;
+  rclcpp::TimerBase::SharedPtr timer_;
+  geometry_msgs::msg::Twist cmd_vel;
+
+};
+
+int main(int argc, char **argv) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<SimuBotDriver>());
+  rclcpp::shutdown();
+  return 0;
+}

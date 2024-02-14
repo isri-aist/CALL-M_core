3eb02a03cbf3352751d726ab4a56def792829715
Noceo200 <onoel2050@gmail.com>
2024-02-14 10:15:17 +0900

    Update

--- robot_ws_ros2/src/call_m_start_all/launch/call_m_simu_start_full.launch.py
+++ robot_ws_ros2/src/call_m_start_all/launch/call_m_simu_start_full.launch.py
@@ -17,7 +17,6 @@ def generate_launch_description():
     nav_type = "on_fly" #'none', 'on_fly' or 'localize'
 
     nav_mode = os.path.join(dir_nav2, 'config', 'nav2_params_omni_simu.yaml') #'diff' or 'omni', path to config file
-    nav_tree = os.path.join(dir_nav2, 'behavior_trees', 'navigate_through_poses_w_replanning_and_recovery_and_assisted_teleop.xml') #behavior tree to use for navigation #navigate_w_replanning_and_recovery.xml 
     map_loc = os.path.join(dir_nav2, 'maps', 'map_test.yaml') #path to the map if using localization
     slam_param = os.path.join(dir_slam, 'config/mapper_params_online_async.yaml') #path to params for slam
 
@@ -25,7 +24,7 @@ def generate_launch_description():
     LAUNCHES
     """
     #Command to launch xterm and execute ROS 2 launch commands
-    cmd_debug = ['xterm', '-fn', 'xft:fixed:size=12', '-geometry', '120x40', '-e', 'ros2', 'launch']
+    cmd_debug = ['xterm', '-fn', 'xft:fixed:size=12', '-geometry', '130x40', '-e', 'ros2', 'launch']
     cmd = ['ros2', 'launch']
     suffix = ['use_sim_time:=true']
 
@@ -35,9 +34,9 @@ def generate_launch_description():
     simu_launch = launch.actions.ExecuteProcess(cmd=cmd + ['call_m_simulation', 'simulation.launch.py'], output='screen')
     display_launch = launch.actions.ExecuteProcess(cmd=cmd + ['call_m_monitor', 'display.launch.py'], output='screen')
     teleop_launch =  launch.actions.ExecuteProcess(cmd=cmd + ['call_m_teleoperation', 'teleop.launch.py'], output='screen')
-    nav2_launch = launch.actions.ExecuteProcess(cmd=cmd_debug + ['call_m_nav2', 'navigation_launch.py','params_file:='+nav_mode, 'default_bt_xml_filename:='+nav_tree] + suffix, output='screen')
-    nav2_launch_loc1 = launch.actions.ExecuteProcess(cmd=cmd_debug + ['call_m_nav2', 'localization_launch.py','params_file:='+nav_mode,'map:='+map_loc] + suffix, output='screen')
-    nav2_launch_loc2 = launch.actions.ExecuteProcess(cmd=cmd_debug + ['call_m_nav2', 'navigation_launch.py','params_file:='+nav_mode,'map_subscribe_transient_local:=true','default_bt_xml_filename:='+nav_tree] + suffix, output='screen')
+    nav2_launch = launch.actions.ExecuteProcess(cmd=cmd_debug + ['call_m_nav2', 'navigation_launch.py','params_file:='+nav_mode] + suffix, output='screen')
+    nav2_launch_loc = launch.actions.ExecuteProcess(cmd=cmd_debug + ['call_m_nav2', 'localization_launch.py','params_file:='+nav_mode,'map:='+map_loc] + suffix, output='screen')
+    #nav2_launch_loc2 = launch.actions.ExecuteProcess(cmd=cmd_debug + ['call_m_nav2', 'navigation_launch.py','params_file:='+nav_mode,'map_subscribe_transient_local:=true'] + suffix, output='screen')
     #ros2 launch nav2_bringup localization_launch.py params_file:=/home/jrlintern/Desktop/work/CNRS_AIST_Work_All/CNRS_AIST_Mobile_Shopping_Robot/robot_ws_ros2/src/call_m_nav2/config/nav2_params _original.yaml map:=/home/jrlintern/Desktop/work/CNRS_AIST_Work_All/CNRS_AIST_Mobile_Shopping_Robot/robot_ws_ros2/src/call_m_nav2/maps/map_test.yaml use_sim_time:=true
     #ros2 launch call_m_nav2 localization_launch.py params_file:=/home/jrlintern/Desktop/work/CNRS_AIST_Work_All/CNRS_AIST_Mobile_Shopping_Robot/robot_ws_ros2/src/call_m_nav2/config/nav2_params_diff_simu.yaml map:=/home/jrlintern/Desktop/work/CNRS_AIST_Work_All/CNRS_AIST_Mobile_Shopping_Robot/robot_ws_ros2/src/call_m_nav2/maps/map_test.yaml use_sim_time:=true
     #ros2 launch call_m_nav2 navigation_launch.py params_file:=/home/jrlintern/Desktop/work/CNRS_AIST_Work_All/CNRS_AIST_Mobile_Shopping_Robot/robot_ws_ros2/src/call_m_nav2/config/nav2_params_diff_simu.yaml map_subscribe_transient_local:=true use_sim_time:=true
@@ -47,6 +46,6 @@ def generate_launch_description():
     if nav_type == "on_fly":
         return LaunchDescription([master_launch,slam_launch,simu_launch,display_launch,teleop_launch,nav2_launch])
     elif nav_type == "localize":
-        return LaunchDescription([master_launch,simu_launch,display_launch,teleop_launch,nav2_launch_loc1,nav2_launch_loc2])
+        return LaunchDescription([master_launch,simu_launch,display_launch,teleop_launch,nav2_launch_loc,nav2_launch])
     else:
         return LaunchDescription([master_launch,slam_launch,simu_launch,display_launch,teleop_launch])
